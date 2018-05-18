776abe7499cc331374bd4cc9b7385a11f335b101
timsli <infracloud@sysetele.com>
2018-05-18 06:00:36 +0200

    Update GetUserSubuidAndSubgid.sh

--- GetUserSubuidAndSubgid.sh
+++ GetUserSubuidAndSubgid.sh
@@ -1,14 +1,16 @@
+
 #!/usr/bin/env bash
 # Read for given username the subgid/subgid and their corresponding count according to /etc/subuid and /etc/subgid files.
 # Params: username
 # Output is: 
 #     subuid:subuidcount
 #     subgid:subgidcount
-set -euxo pipefail 
+set -o errexit
+set -o pipefail
+set -o nounset
 if [[ $# -ne 1 ]]
 then
   printf "The username is required as argument to run this script.\n\tUsage:$0 timsli\n"
   exit -1
 fi
-username=$1;  grep $username /etc/sub*id | cut -d":" -f3-4
-                                                                  
+username=$1; grep $username /etc/sub*id | cut -d":" -f3-4
