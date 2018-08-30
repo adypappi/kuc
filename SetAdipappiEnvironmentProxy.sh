1aaf6c9c18e5f23ca54cffe64a74a6be3eb527aa
timsli <infracloud@sysetele.com>
2018-08-30 17:10:16 +0200

    Update SetAdipappiEnvironmentProxy.sh

--- SetAdipappiEnvironmentProxy.sh
+++ SetAdipappiEnvironmentProxy.sh
@@ -1,14 +1,16 @@
 
 #!/usr/bin/env bash
 # In adypappi ndc architecture each datacenter has a proxy fw umt server. 
-# This script generate the other script /etc/profile.d/adipappi_proxy.sh
+# This script generate the other script /etc/profile.d/adipappi_proxy.sh on each server to allow internet access. 
+# Adapt the content and 
 export  PapiProxyEnvFile=/etc/profile.d/adipappi_proxy.sh
 export PROXY_FQDN=pfwprxlnx1.adipappi.int
 export PROXY_PORT=8989
 export PRX_SRV_URL="http://${PROXY_FQDN}:${PROXY_PORT}"
+export NDC_INTERNAL_DOMAINE_NAME=adipappi.int
 printf "#Configuration of Adipappi NDC PROXY SERVER\n" >> $PapiProxyEnvFile
 chmod a+x $PapiProxyEnvFile
 printf  "export http_proxy=$PRX_SRV_URL\n" >> $PapiProxyEnvFile
 printf  "export https_proxy=$PRX_SRV_URL\n" >> $PapiProxyEnvFile
 printf  "export ftp_proxy=$PRX_SRV_URL\n" >> $PapiProxyEnvFile
-printf  "export no_proxy=localhost,127.0.0.1,172.16.224.0/20,172.16.240.0/21,172.16.3.0/24\n" >> $PapiProxyEnvFile
+printf  "export no_proxy=localhost,127.0.0.1,.${NDC_INTERNAL_DOMAINE_NAME}\n" >> $PapiProxyEnvFile
