84281b9dc4e43c87ca8266f937573d4ba0bc6b79
NDC1_PROTO <infracloud@sysetele.com>
2022-09-30 02:43:33 +0200

    Create ListRecursivelyFolderContents.sh
    List all files and subfolders of a given base folder recursively. Each line correspond to a full qualified path of a subfolder and  its contents.

---
+++ ListRecursivelyFolderContents.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+#
+# List recursively given folder subfolders and files. 
+IFS_OLD=$IFS; 
+IFS=;
+for fl in $(ls -Ra -1 ${folder} | sed -r 's|^\s*$|;|g' | grep -Pv "\./|\.\./"  | xargs)
+do 
+  IFS=$OLD_IFS
+  ## The current folder FQP end with ':'  the rest if the contentn of current folder
+  ## The subfolder end with /
+  echo "$fl" 
+  IFS=;
+done 
+IFS=$OLD_IFS
